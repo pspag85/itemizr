4ef23d7156707b6cc22b5023d24ddc0e5362debf
jbprojectlab <James.Byrd1265@gmail.com>
2019-07-05 14:15:48 -0400

    style: list link flex

--- client/components/list.js
+++ client/components/list.js
@@ -9,22 +9,20 @@ const List = ({id, date, name, lastEditedBy, deleteList}) => {
   const closeMenu = () => setMenuState('closed')
   return (
     <div className='row bg-white'>
-      <Link to={`/lists/${id}/order`}>
-        <div>
-          {date && <div className='column'>
-            <h4 className='light-font'>{date.slice(0,10)}</h4>
-          </div>}
-          <div className='column'>
-            <h4 className='light-font'>{name}</h4>
-          </div>
-          {lastEditedBy && <div className='column'>
-            <h4 className='light-font'>{lastEditedBy}</h4>
-          </div>}
-          {deleteList && <div className='column pointer' onClick={() => deleteList(id)}>
-            <img className='delete-icon' src='/img/delete.png'/>
-            <h4 className='delete-txt light-font'>Delete this list</h4>
-          </div>}
+      <Link to={`/lists/${id}/order`} className='list-link'>
+        {date && <div className='column'>
+          <h4 className='light-font'>{date.slice(0,10)}</h4>
+        </div>}
+        <div className='column'>
+          <h4 className='light-font'>{name}</h4>
         </div>
+        {lastEditedBy && <div className='column'>
+          <h4 className='light-font'>{lastEditedBy}</h4>
+        </div>}
+        {deleteList && <div className='column pointer' onClick={() => deleteList(id)}>
+          <img className='delete-icon' src='/img/delete.png'/>
+          <h4 className='delete-txt light-font'>Delete this list</h4>
+        </div>}
       </Link>
       <div className='list-menu-container column pointer bg-white' onClick={openMenu}>
         <img src='/img/more-vert.png' />
