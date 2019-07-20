ecf14acc54b90d16eaa91655bc74922aff9472d9
jbprojectlab <James.Byrd1265@gmail.com>
2019-07-20 15:02:43 -0400

    bug: initial item occasionally not appearing on list creation

--- client/components/edit-items.js
+++ client/components/edit-items.js
@@ -1,4 +1,4 @@
-import React, {Component, Fragment} from 'react'
+import React, {useEffect, Fragment} from 'react'
 import {withRouter, Link} from 'react-router-dom'
 import {connect} from 'react-redux'
 import axios from 'axios'
@@ -10,21 +10,20 @@ import ColHeader from './col-header'
 import {getList, getItems, addItem, removeItem, saveItems, cancelUpdate} from '../store'
 import '../css/edit-items.css'
 
-class EditItems extends Component {
+const EditItems = ({user, getCurrentList, currentList, loadItems, items, deleteItem, saveChanges, cancelChanges, history, location}) => {
+  if(!user.id) history.push('/')
+  const {pathname} = location
+  const listId = pathname.split('/')[2]
 
-  componentDidMount() {
-    const {user, getCurrentList, loadItems, history, location} = this.props
-    if(!user.id) history.push('/')
-    const {pathname} = location
-    const listId = pathname.split('/')[2]
+  useEffect(() => {
     getCurrentList(listId)
-    loadItems(listId)
-  }
+  }, [])
 
-  addNewItem = async () => {
-    const {items, createItem, location} = this.props
-    const {pathname} = location
-    const listId = pathname.split('/')[2]
+  useEffect(() => {
+    loadItems(listId)
+  }, [loadItems])
+
+  const addNewItem = async () => {
     try {
       const {data} = await axios.get('/api/items')
       let id = data
@@ -37,47 +36,42 @@ class EditItems extends Component {
     }
   }
 
-  cancelEdit = () => {
-    const {currentList, cancelChanges, history} = this.props
+  const cancelEdit = () => {
     cancelChanges(currentList.id)
     history.push(`/lists/${currentList.id}`)
   }
 
-  render() {
-    const {addNewItem, cancelEdit} = this
-    const {currentList, items, logoutUser, deleteItem, saveChanges} = this.props
-    const itemsArr = !Array.isArray(items) ? [{}] : items
-    return currentList ? (
-      <Fragment>
-        <UserPage />
-        <div id='edit-items-body'>
-          <div className='header row font-20'>
-          <h3>{currentList.name}</h3>
-          </div>        
-          <div className='col-header row secondary-txt'>
-            <ColHeader headers={['ITEM', 'ON HAND', 'PAR', 'ORDER QTY']} />
-          </div>      
-          <div className='edit-items-container bg-white box-shadow'>
-            {itemsArr.map(({id, name, onHand, par, orderQty}, index) => (
-              <EditItem
-                id={id}
-                name={name}
-                onHand={onHand}
-                par={par}
-                orderQty={orderQty}
-                deleteItem={deleteItem}
-              />
-            ))}
-            <AddItemButton listId={currentList.id} addNewItem={addNewItem} />
-          </div>
-          <div className='save'>
-            <button className='action-btn white bg-blue pointer' onClick={() => saveChanges(currentList.id, itemsArr)}>SAVE CHANGES</button>
-            <h4 className='cancel-btn pointer light-font' onClick={cancelEdit}>CANCEL</h4>
-          </div>
+  const itemsArr = !Array.isArray(items) ? [{}] : items
+  return currentList ? (
+    <Fragment>
+      <UserPage />
+      <div id='edit-items-body'>
+        <div className='header row font-20'>
+        <h3>{currentList.name}</h3>
+        </div>        
+        <div className='col-header row secondary-txt'>
+          <ColHeader headers={['ITEM', 'ON HAND', 'PAR', 'ORDER QTY']} />
+        </div>      
+        <div className='edit-items-container bg-white box-shadow'>
+          {items.map(({id, name, onHand, par, orderQty}, index) => (
+            <EditItem
+              id={id}
+              name={name}
+              onHand={onHand}
+              par={par}
+              orderQty={orderQty}
+              deleteItem={deleteItem}
+            />
+          ))}
+          <AddItemButton listId={currentList.id} addNewItem={addNewItem} />
         </div>
-      </Fragment>
-    ) : null
-  }
+        <div className='save'>
+          <button className='action-btn white bg-blue pointer' onClick={() => saveChanges(currentList.id, items)}>SAVE CHANGES</button>
+          <h4 className='cancel-btn pointer light-font' onClick={cancelEdit}>CANCEL</h4>
+        </div>
+      </div>
+    </Fragment>
+  ) : null
 }
 
 const mapStateToProps = ({user, lists, items}) => ({user, currentList: lists[0], items})
