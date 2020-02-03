14cdd23e81fffba259ce8a6901fa57ea7497ea79
James Byrd <James.Byrd@pw.utc.com>
2020-02-03 13:45:52 -0500

    style: change color scheme

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,7 +3,7 @@ import {withRouter, Link} from 'react-router-dom'
 import '../css/navbar.css'
 
 const Navbar = withRouter(() => (
-  <div id='navbar' className='bg-white nav-wdth'>
+  <div id='navbar' className='bg-lt-blue nav-wdth'>
     <Link to='/lists'>Inventory</Link>
     <Link to='/suppliers'>Suppliers</Link>
     <Link to='/users'>Admin Settings</Link>
