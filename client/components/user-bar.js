9bb0baf9cd86b83dca707796c23fe3c4fbf4e9f6
James Byrd <James.Byrd@pw.utc.com>
2020-02-05 17:32:42 -0500

    style: header text

--- client/components/user-bar.js
+++ client/components/user-bar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Redirect, Link} from 'react-router-dom'
+import {withRouter, Redirect, Link} from 'react-router-dom'
 import {connect} from 'react-redux'
 import {logout} from '../store'
 import LogoHeader from './logo-header'
@@ -7,20 +7,22 @@ import Navbar from './navbar'
 import NavCart from './nav-cart'
 import '../css/user-bar.css'
 
-const UserBar = ({user}) => {
+const UserBar = withRouter(({user, location}) => {
   if(!user.id) {
     return <Redirect to='/' />
   }
 
+  const header = location.pathname.split('/')[1]
   return (
     <div id='user-bar'>
       <div id='header-container'>
+        <h2>{header}</h2>
         <NavCart />
       </div>
       <Navbar />
     </div>
   )
-}
+})
 
 const mapStateToProps = ({user}) => ({user})
 
