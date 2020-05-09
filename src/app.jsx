89b4a37b4ae2ad7f2937522b6a28d1c2320ad9f6
James Byrd <james.byrd@prattwhitney.com>
2020-05-09 14:34:15 -0400

    fix: get user when app mounts

--- src/app.jsx
+++ src/app.jsx
@@ -10,15 +10,15 @@ import './css/app.css';
 
 class App extends Component {
   async componentDidMount() {
-    const {history, location} = this.props
-    const {pathname} = location
-    let path = pathname === '/' ? '/products' : pathname
+    const {history, location} = this.props;
+    const {pathname} = location;
+    let path = pathname === '/' ? '/products' : pathname;
     try {
-      const user = await store.dispatch(getMe())
-      path = !user ? '/login' : path
-      history.push(`${path}`)
-    } catch(err) {
-      console.error(err)
+      const user = await store.dispatch(getMe());
+      path = !user ? '/login' : path;
+      history.push(`${path}`);
+    } catch (err) {
+      console.error(err);
     }
   }
 
