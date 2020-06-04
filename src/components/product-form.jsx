eb5ab6d6bdbfa5fded14931b9c296adefdf21c1d
James Byrd <james.byrd@prattwhitney.com>
2020-06-04 13:51:29 -0400

    style: responsive product form labels

--- src/components/product-form.jsx
+++ src/components/product-form.jsx
@@ -49,55 +49,86 @@ const ProductForm = ({
     <div className="product-form-wrapper">
       <div className="product-form">
         <form className="product-form-inputs row" onSubmit={handleSubmit}>
+          <label className="hidden-label" for="name">
+            Name
+          </label>
           <input
             placeholder={name || 'Name'}
             name="name"
             value={name}
             onChange={handleChange}
           />
+          <label className="hidden-label" for="productNumber">
+            Product No.
+          </label>
           <input
             id="product-number"
             placeholder={productNumber || 'No.'}
             title="Product number is read only" // TODO: show on click
             disabled
           />
+          <label className="hidden-label" for="category">
+            Category
+          </label>
           <Select
             currentSelection={category}
             defaultSelection="Category"
             toggleOptions={toggleCategoryOptions}
           />
+          <label className="hidden-label" for="vendor">
+            Vendor
+          </label>
           <Select
             currentSelection={vendorName}
             defaultSelection="Vendor"
             toggleOptions={toggleVendorOptions}
           />
           <div className="price-input">
+            <label className="hidden-label" for="price">
+              Price
+            </label>
             <input
               placeholder={price}
               name="price"
               value={price}
               onChange={handleChange}
             />
-            <div className="qty-wrapper">
-              <input
-                placeholder={quantity}
-                name="quantity"
-                value={quantity}
-                onChange={handleChange}
-              />
-              <Select
-                currentSelection={unit}
-                defaultSelection="Unit"
-                toggleOptions={toggleUnitOptions}
-              />
+            <div className="unit-input">
+              <div className="flex-item">
+                <label className="hidden-label" for="quantity">
+                  Quantity
+                </label>
+                <input
+                  placeholder={quantity}
+                  name="quantity"
+                  value={quantity}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="flex-item">
+                <label className="hidden-label" for="unit">
+                  Unit
+                </label>
+                <Select
+                  currentSelection={unit}
+                  defaultSelection="Unit"
+                  toggleOptions={toggleUnitOptions}
+                />
+              </div>
             </div>
           </div>
+          <label className="hidden-label" for="par">
+            Par
+          </label>
           <input
             placeholder={par}
             name="par"
             value={par}
             onChange={handleChange}
           />
+          <label className="hidden-label" for="onHand">
+            On-hand
+          </label>
           <input
             placeholder={onHand}
             name="onHand"
